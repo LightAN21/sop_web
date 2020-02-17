b4d6da4b35a82b2c5173d193ef0b8fa9ade87a5d
LightAN21 <bosengt@gmail.com>
2020-02-16 18:02:05 -0800

    add

--- client/freatues/multi_condition.js
+++ client/freatues/multi_condition.js
@@ -1,15 +1,15 @@
 
 function multi_condition() {
     var condition = [
-        has_c1,
-        has_c2,
-        has_c3_1,
-        has_c3_2,
-        has_c4,
-        has_c5,
-        has_c6,
-        has_c7,
-        has_c8,
+        {check: has_c0, name: 'c0', is_checked: document.getElementById("c0_checkbox").checked},
+        {check: has_c1, name: 'c1', is_checked: document.getElementById("c1_checkbox").checked},
+        {check: has_c2, name: 'c2', is_checked: document.getElementById("c2_checkbox").checked},
+        {check: has_c3_1, name: 'c3_1', is_checked: document.getElementById("c3_1_checkbox").checked},
+        {check: has_c3_2, name: 'c3_2', is_checked: document.getElementById("c3_2_checkbox").checked},
+        {check: has_c4, name: 'c4', is_checked: document.getElementById("c4_checkbox").checked},
+        {check: has_c5, name: 'c5', is_checked: document.getElementById("c5_checkbox").checked},
+        {check: has_c6, name: 'c6', is_checked: document.getElementById("c6_checkbox").checked},
+        {check: has_c7, name: 'c7', is_checked: document.getElementById("c7_checkbox").checked},
     ];
     clear_result_area();
     progress_bar_show_msg('Searching...');
@@ -21,7 +21,7 @@ function multi_condition() {
     for (var i = 0; i < com.length; i++) {
         var comp = com[i];
         for (var j = 0; j < condition.length; j++) {
-            if (condition[j](comp)) {
+            if (condition[j].is_checked && condition[j].check(comp)) {
                 list.push(comp.name);
                 break ;
             }
