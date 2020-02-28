f04cd8504968bd9cb005f6f429a01f959e91cd88
Jonathan Tsai <jtsai@e1z2r3p37.42.us.org>
2020-02-27 23:38:37 -0800

    add

--- client/charts/charts.js
+++ client/charts/charts.js
@@ -1,3 +1,6 @@
+var canvasArea = document.getElementById("canvas1");
+var drawArea = canvasArea.getContext("2d");
+
 function get_chart_com_name(){
     var n = document.getElementById('chart_com_name').value;
 
@@ -7,4 +10,16 @@ function get_chart_com_name(){
 
 function show_chart(){
     var name = get_chart_com_name();
+    var width = canvasArea.width, height = canvasArea.height;
+
+    drawArea.clearRect(0, 0, width, height);
+    drawArea.fillStyle = 'black';
+    drawArea.fillRect(0, 0, width, height);
+
+    drawArea.fillStyle = 'gray';
+    drawArea.fillRect(100, 200, 5, 30);
+    
+    drawArea.fillRect(102, 180, 1, 80);
+
+
 }
